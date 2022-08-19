9c7fafbac16d340ec47d7b1cca8489ccfe6467cb
Pablo Orduña <pablo@labsland.com>
2022-08-19 13:10:48 -0700

    Start storing data from gnuradio

--- datauploader/views.py
+++ datauploader/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, request, jsonify
 
 from datauploader import redis_store
 
@@ -11,7 +12,57 @@ def index():
 
 api_blueprint = Blueprint('api', __name__)
 
-@api_blueprint.route('/upload')
-def upload():
-    result = redis_store.get('result')
-    return f"Uploading. btw, in redis I found: {result}"
+@api_blueprint.route('/upload/sessions/<session_identifier>/devices/<device_identifier>/blocks/<block_identifier>', methods=['GET', 'POST'])
+def device_block_data(session_identifier, device_identifier, block_identifier):
+    """
+    This method does not know anything about the widgets themselves, but just stores the information 
+
+    """
+    if request.method == 'GET':
+        return jsonify(success=False, message="Not implemented yet")
+
+    # else: if POST
+
+
+    # 
+    # {
+    #     'block_type': 'relia_time_sink_x',
+    #     'type': 'complex64',
+    #     'params': {
+    #         e.g.,
+    #         'nop': '1024',
+    #     }
+    #     'data': {
+    #         e.g.,
+    #         'streams': {
+    #             '0': {
+    #                 'real': [1,2,3],
+    #                 'imag': [1,2,3],
+    #             }
+    #         }
+    #     }
+    # }
+    #
+    request_data = request.get_json(force=True, silent=True)
+    print(request_data)
+
+    block_key = f'relia:data-uploader:sessions:{session_identifier}:devices:{device_identifier}:blocks:{block_identifier}'
+    blocks_key = f'relia:data-uploader:sessions:{session_identifier}:devices:{device_identifier}:blocks'
+    devices_key = f'relia:data-uploader:sessions:{session_identifier}:devices'
+    sessions_key = f'relia:data-uploader:sessions'
+
+    pipeline = redis_store.pipeline()
+    pipeline.rpush(block_key, json.dumps(request_data))
+
+    pipeline.sadd(blocks_key, block_identifier)
+    pipeline.sadd(devices_key, device_identifier)
+
+    # Probably wrong
+    pipeline.sadd(sessions_key, session_identifier)
+    
+    # Expire in 10 minutes (unless someone adds more data here)
+    for key in (block_key, blocks_key, devices_key, sessions_key):
+        pipeline.expire(key, 60 * 10)
+    pipeline.execute()
+
+    return jsonify(success=True)
