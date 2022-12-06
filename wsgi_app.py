bff88cb6aae7ed3971b57406252183565abfa484
Pablo Orduña <pablo@labsland.com>
2022-12-06 10:35:57 -0800

    Add wsgi_app

---
+++ wsgi_app.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+project_dir = os.path.abspath(os.path.dirname(__file__))
+
+sys.path.insert(0, project_dir)
+os.chdir(LAB_DIR)
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
+
+sys.stdout = open('logs/stdout.txt', 'a')
+sys.stderr = open('logs/stderr.txt', 'a')
+
+from reliabackend import create_app
+application = create_app(os.environ['FLASK_CONFIG'])
+
+import logging
+
+file_handler = logging.FileHandler(filename='logs/errors.log')
+file_handler.setLevel(logging.INFO)
+application.logger.addHandler(file_handler)
