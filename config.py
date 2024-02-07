c874ed2c126c1ad8aa8338c58a10d39a6e76f8ba
Pablo Orduña <pablo@labsland.com>
2024-02-06 17:59:06 -0800

    Add cookie-related variables

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     REDIS_URL = os.environ.get('REDIS_URL') or "redis://localhost/0"
+    SCRIPT_NAME = os.environ.get('SCRIPT_NAME') or '/'
+    SESSION_COOKIE_PATH = os.environ.get('SESSION_COOKIE_PATH') or '/'
 
 class DevelopmentConfig(Config):
     DEBUG = True
